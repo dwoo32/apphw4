ed173e46fafc271f025e9275d2402b0a0808c82e
KimDongWoo <dw81770@gmail.com>
2024-04-18 21:23:17 +0900

    app.4.2

--- app/src/main/java/com/example/animalpic/MainActivity.java
+++ app/src/main/java/com/example/animalpic/MainActivity.java
@@ -1,83 +1,36 @@
 package com.example.animalpic;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-
+import android.os.Bundle;
+import android.widget.TabHost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
 
-    TextView text1, text2;
-    CheckBox chkAgree;
-    RadioGroup rGroup1;
-    RadioButton rdoDog, rdoCat, rdoRabbit;
-    Button btnOk;
-    ImageView imgPet;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("애완동물 사진 보기");
 
-        text1 = (TextView) findViewById(R.id.Text1);
-        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+        TabHost tabHost = findViewById(R.id.tabHost);
+        tabHost.setup();
 
-        text2 = (TextView) findViewById(R.id.Text2);
-        rGroup1 = (RadioGroup) findViewById(R.id.RGroup1);
-        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
-        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
-        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+        TabHost.TabSpec tabDog = tabHost.newTabSpec("Dog").setIndicator("강아지");
+        tabDog.setContent(R.id.tabDog);
+        tabHost.addTab(tabDog);
 
-        btnOk = (Button) findViewById(R.id.BtnOk);
-        imgPet = (ImageView) findViewById(R.id.ImgPet);
+        TabHost.TabSpec tabCat = tabHost.newTabSpec("Cat").setIndicator("고양이");
+        tabCat.setContent(R.id.tabCat);
+        tabHost.addTab(tabCat);
 
-        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (chkAgree.isChecked()) { //default가 true이기 때문에 '==true'는 삭제
-                    text2.setVisibility(View.VISIBLE);
-                    rGroup1.setVisibility(View.VISIBLE);
-                    btnOk.setVisibility(View.VISIBLE);
-                    imgPet.setVisibility(View.VISIBLE);
-                } else {
-                    text2.setVisibility(View.INVISIBLE);
-                    rGroup1.setVisibility(View.INVISIBLE);
-                    btnOk.setVisibility(View.INVISIBLE);
-                    imgPet.setVisibility(View.INVISIBLE);
-                }
-            }
-        });
+        TabHost.TabSpec tabRabbit = tabHost.newTabSpec("Rabbit").setIndicator("토끼");
+        tabRabbit.setContent(R.id.tabRabbit);
+        tabHost.addTab(tabRabbit);
 
-        btnOk.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int checkedRadioButtonId = rGroup1.getCheckedRadioButtonId(); //getCheckedRadioButtonId()는 현재 라디오 그룹에서 선택된 라디오 버튼의 아이디 값을 반환
-                if (checkedRadioButtonId == R.id.RdoDog) {
-                    imgPet.setImageResource(R.drawable.dog); //이미지를 출력하는 메소드
-                } else if (checkedRadioButtonId == R.id.RdoCat) {
-                    imgPet.setImageResource(R.drawable.cat);
-                } else if (checkedRadioButtonId == R.id.RdoRabbit) {
-                    imgPet.setImageResource(R.drawable.rabbit);
-                } else {
-                    Toast.makeText(getApplicationContext(), "동물 먼저 선택하세요", Toast.LENGTH_SHORT).show();
-                }
-            } //깨꿀인 부분 ~ alt enter 레전드 최고임~ 예외처리도 해주고 구문 변환도 뚝딱뚜깎딲
-        });
+        TabHost.TabSpec tabHorse = tabHost.newTabSpec("Horse").setIndicator("말");
+        tabHorse.setContent(R.id.tabHorse);
+        tabHost.addTab(tabHorse);
 
-
+        tabHost.setCurrentTab(0);
     }
 }
